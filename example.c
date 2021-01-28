b2cbf9fc8629e830b15dce47fb4376f1919cf63f
THON SEY <seythun577@gmail.com>
2021-01-28 21:57:50 +0700

    Hello

---
+++ example.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    void main()
+    {
+        int  j, sum = 0;
+        for (j = 1; j <= 50; j++)
+        {
+            sum = sum + j;
+        }
+        printf("Sum = %4d\n", sum);
+    }
